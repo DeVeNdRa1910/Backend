55fd54e742f21366788918b54a1502fc153d5c43
Devendra <devendrav1910@gmail.com>
2024-05-03 15:42:17 +0530

    learn about middleware

--- lec-4/script.js
+++ lec-4/script.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express()
 
+app.use(function(req, res, next){
+    console.log("Middleware chala");
+    next()
+})
+//is server par koi bhi route se pehle app.use() chalega  (ye middleware hai) next() ke through ham request ko age forword karte hai
+app.use(function(req, res, next){
+    console.log("Middleware chala ek oor baar");
+    next()
+})
+
+
+
 // now we can set routes
 // app.get(route, requestHandler) -> route and requestHandler both are middleware
 app.get('/', function (req, res) {
@@ -9,6 +21,16 @@ app.get('/', function (req, res) {
 
 app.get('/youtube', function(req, res){
     res.send("<h1>DV MUSIC</h1>")
+})
+
+app.get("/about", function(req, res){
+    // res.send("This About page")
+    return next(new Error("Somthing went wrong"))
+})
+
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send("Sopmthing broke!")
 })
 
 app.listen(3000)
